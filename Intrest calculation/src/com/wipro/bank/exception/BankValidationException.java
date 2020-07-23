0fc435fd2adaf877a4fded6d25257803ad510727
User <User@DESKTOP-USNQFOT>
2020-07-23 15:23:59 +0530

    second commit

---
+++ Intrest calculation/src/com/wipro/bank/exception/BankValidationException.java
@@ -0,0 +1,16 @@
+package com.wipro.bank.exception;
+
+ 
+
+public class BankValidationException extends Exception{
+    public String toString()
+    {
+        return "Invalid Data";
+
+ 
+
+    }
+public BankValidationException(String str) {
+    super(str);
+    }
+}
